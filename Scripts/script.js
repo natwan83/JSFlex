f3bd635a8d2242dd7f89f37be0cf026800da9628
Natasha Wanyonyi <natwan83@hamiltonchristian.school.nz>
2020-08-24 12:55:05 +1200

    linked Json
    Signed-off-by: Natasha Wanyonyi <natwan83@hamiltonchristian.school.nz>

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,27 @@
-const panels = document.querySelectorAll('.panel');
+
+
+function loadData() {
+  fetch('Scripts/data.json')
+  .then(resp => resp.json())
+  .then(data => {
+
+    let panels = data.images.map((panel, i) => {
+
+      return `
+      <div class=panel panel${1} style="background-image": url${panel.image}}">
+      <p>${panel.text_top}</p>
+      <p>${panel.text_middle}</p>
+      <p>${panel.text_bottom}</p>
+      </div>
+      `
+
+  })
+  
+  const panels = document.querySelectorAll('.panel');
+
+  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+  panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+}
 
 function toggleOpen() {
   console.log('Hello');
@@ -12,5 +35,3 @@ function toggleActive(e) {
   }
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
